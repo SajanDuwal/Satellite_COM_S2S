8ad27d826590a9ca310aab24950da58b20eeabd1
sajan <02790sajanduwal@gmail.com>
2024-09-23 16:31:24 +0545

    Digipeater mode error handled

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -356,40 +356,39 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
-int main(void)
-{
-  /* USER CODE BEGIN 1 */
+ * @brief  The application entry point.
+ * @retval int
+ */
+int main(void) {
+	/* USER CODE BEGIN 1 */
 
-  /* USER CODE END 1 */
+	/* USER CODE END 1 */
 
-  /* MCU Configuration--------------------------------------------------------*/
+	/* MCU Configuration--------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
+	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+	HAL_Init();
 
-  /* USER CODE BEGIN Init */
+	/* USER CODE BEGIN Init */
 
-  /* USER CODE END Init */
+	/* USER CODE END Init */
 
-  /* Configure the system clock */
-  SystemClock_Config();
+	/* Configure the system clock */
+	SystemClock_Config();
 
-  /* USER CODE BEGIN SysInit */
+	/* USER CODE BEGIN SysInit */
 
-  /* USER CODE END SysInit */
+	/* USER CODE END SysInit */
 
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_DMA_Init();
-  MX_USART2_UART_Init();
-  MX_LPUART1_UART_Init();
-  MX_USART1_UART_Init();
-  MX_SubGHz_Phy_Init();
-  MX_TIM2_Init();
-  /* USER CODE BEGIN 2 */
+	/* Initialize all configured peripherals */
+	MX_GPIO_Init();
+	MX_DMA_Init();
+	MX_USART2_UART_Init();
+	MX_LPUART1_UART_Init();
+	MX_USART1_UART_Init();
+	MX_SubGHz_Phy_Init();
+	MX_TIM2_Init();
+	/* USER CODE BEGIN 2 */
 
 	HAL_TIM_Base_Start(&htim2);
 
@@ -450,15 +449,15 @@ int main(void)
 
 	HAL_UART_Receive_DMA(&hlpuart1, OBC_UART_RX, obc_plen);
 
-  /* USER CODE END 2 */
+	/* USER CODE END 2 */
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
+	/* Infinite loop */
+	/* USER CODE BEGIN WHILE */
 	while (1) {
-    /* USER CODE END WHILE */
-    MX_SubGHz_Phy_Process();
+		/* USER CODE END WHILE */
+		MX_SubGHz_Phy_Process();
 
-    /* USER CODE BEGIN 3 */
+		/* USER CODE BEGIN 3 */
 
 		delay_us(500000);
 
@@ -535,7 +534,7 @@ int main(void)
 				main_gs_cmd[i] = gs_cmd_buff[i];
 			}
 
-			if (COUNT_BEACON == 1) {
+			if (COUNT_BEACON == 1) { // first beacon tx
 
 				if ((main_gs_cmd[18] == 0xDF && main_gs_cmd[19] == 0xAB
 						&& main_gs_cmd[20] == 0xD1) // Enable Digipeater Mission
@@ -557,7 +556,7 @@ int main(void)
 					memset(main_gs_cmd, '\0', sizeof(main_gs_cmd));
 
 				} else if (main_gs_cmd[18] == 0xFD && main_gs_cmd[19] == 0xBA
-						&& main_gs_cmd[20] == 0xD0) {
+						&& main_gs_cmd[20] == 0xD0) {  // Digipeater Disable
 
 					BEACON_COUNT = 2;
 					OBC_SUCCESS_DATA_RX_FLAG = 0;
@@ -663,6 +662,46 @@ int main(void)
 
 						OBC_SUCCESS_DATA_RX_FLAG = 1;
 
+					} else if (main_gs_cmd[18] == 0xDF
+							&& main_gs_cmd[19] == 0xAB
+							&& main_gs_cmd[20] == 0xD1) { //Digipeater on
+
+						BEACON_COUNT = 0;
+						OBC_SUCCESS_DATA_RX_FLAG = 0;
+						DIGIPEATER_STATUS = 1;
+
+						myDebug("\n-------> Digipeater MODE ON\n\n");
+
+						OBC_UART_RX[0] = 0x53;	//header
+						OBC_UART_RX[1] = 0xac;	//packet_ type
+						OBC_UART_RX[2] = 0x04;	//length of info
+						OBC_UART_RX[3] = 0x02;	//mcu_no
+						OBC_UART_RX[4] = 0x01;	//Digipeater ON
+						OBC_UART_RX[5] = 0xdd;	//Digipeater prefix
+						obc_ilen = OBC_UART_RX[2];
+
+						myDebug("\nCommand sent to OBC, Length: %d bytes\r\n",
+								gs_cmd_len);
+
+						if (HAL_UART_Transmit(&huart2, main_gs_cmd,
+								sizeof(main_gs_cmd), 2000) == HAL_OK
+								|| HAL_UART_Transmit(&hlpuart1, main_gs_cmd,
+										sizeof(main_gs_cmd), 2000)) {
+
+							for (int i = 0; i < gs_cmd_len; i++) {
+								myDebug("%02x ", main_gs_cmd[i]);
+							}
+
+							myDebug("\r\n");
+
+							memset(main_gs_cmd, '\0', sizeof(main_gs_cmd));
+							memset(rx_buffer, '\0', sizeof(rx_buffer));
+							memset(crc_buff, '\0', sizeof(crc_buff));
+							memset(gs_cmd_buff, '\0', sizeof(gs_cmd_buff));
+						}
+
+						OBC_SUCCESS_DATA_RX_FLAG = 1;
+
 					} else if (main_gs_cmd[18] == 0x53
 							&& main_gs_cmd[19] == 0x02
 							&& main_gs_cmd[20] == 0x53) { // if digipeater packet
@@ -687,7 +726,7 @@ int main(void)
 
 							myDebug(
 									"\n\n_____DIGIPEATER_OBC__RECEIVER_____\r\n");
-
+							DIGIPEATER_RX_FLAG = 1;
 						}
 					} else {
 						myDebug("\n********GS Command failed: 0x%x\r\n");
@@ -752,7 +791,6 @@ int main(void)
 						BEACON_COUNT = 0;
 						OBC_SUCCESS_DATA_RX_FLAG = 0;
 						DIGIPEATER_STATUS = 1;
-						DIGIPEATER_RX_FLAG = 1;
 
 						myDebug("\n-------> Digipeater MODE ON\n\n");
 
@@ -837,54 +875,50 @@ int main(void)
 
 		HAL_UART_Receive_DMA(&hlpuart1, OBC_UART_RX, obc_plen);
 	}
-  /* USER CODE END 3 */
+	/* USER CODE END 3 */
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
-void SystemClock_Config(void)
-{
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+ * @brief System Clock Configuration
+ * @retval None
+ */
+void SystemClock_Config(void) {
+	RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
 
-  /** Configure the main internal regulator output voltage
-  */
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+	/** Configure the main internal regulator output voltage
+	 */
+	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS_PWR;
-  RCC_OscInitStruct.HSEDiv = RCC_HSE_DIV1;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLM = RCC_PLLM_DIV2;
-  RCC_OscInitStruct.PLL.PLLN = 6;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
+	/** Initializes the CPU, AHB and APB buses clocks
+	 */
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+	RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS_PWR;
+	RCC_OscInitStruct.HSEDiv = RCC_HSE_DIV1;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+	RCC_OscInitStruct.PLL.PLLM = RCC_PLLM_DIV2;
+	RCC_OscInitStruct.PLL.PLLN = 6;
+	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+	RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
+	RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+		Error_Handler();
+	}
 
-  /** Configure the SYSCLKSource, HCLK, PCLK1 and PCLK2 clocks dividers
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK3|RCC_CLOCKTYPE_HCLK
-                              |RCC_CLOCKTYPE_SYSCLK|RCC_CLOCKTYPE_PCLK1
-                              |RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
-  RCC_ClkInitStruct.AHBCLK3Divider = RCC_SYSCLK_DIV1;
+	/** Configure the SYSCLKSource, HCLK, PCLK1 and PCLK2 clocks dividers
+	 */
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK3 | RCC_CLOCKTYPE_HCLK
+			| RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+	RCC_ClkInitStruct.AHBCLK3Divider = RCC_SYSCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
-  {
-    Error_Handler();
-  }
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) {
+		Error_Handler();
+	}
 }
 
 /* USER CODE BEGIN 4 */
@@ -1066,17 +1100,16 @@ void DioIrqHndlr(RadioIrqMasks_t radioIrq) {
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
-{
-  /* USER CODE BEGIN Error_Handler_Debug */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
+void Error_Handler(void) {
+	/* USER CODE BEGIN Error_Handler_Debug */
 	/* User can add his own implementation to report the HAL error return state */
 	__disable_irq();
 	while (1) {
 	}
-  /* USER CODE END Error_Handler_Debug */
+	/* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
